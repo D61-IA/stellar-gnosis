9608d7e1fcf3af7b0238193eee4d72a2b3203b41
PantelisElinas <pantelis.elinas@data61.csiro.au>
2019-10-30 15:23:43 +1100

    Removed old and invalid Neo4j, unit tests.

--- gnosis/catalog/test_author_views.py
+++ gnosis/catalog/test_author_views.py
@@ -8,7 +8,6 @@ class PersonViewsTestCase(TestCase):
     def setUp(self):
         #
         self.user = User.objects.create_user(username='testuser', password='12345')
-        #login = self.client.login(username='testuser', password='12345')
 
         Person.objects.create(first_name='Pantelis', last_name='Elinas', created_by=None)
         Person.objects.create(first_name='Fiona', middle_name='Anne', last_name='Elliott', created_by=self.user)
@@ -39,6 +38,34 @@ class PersonViewsTestCase(TestCase):
         response = self.client.get(reverse("person_update", kwargs={'id': person.id}))
         self.assertEqual(response.status_code, 200)
 
+        # Logout
+        self.client.logout()
+
     def test_person_delete(self):
         """ Only an admin user can delete an author """
-        pass
+        person = Person.objects.get(first_name='Fiona')
+        response = self.client.get(reverse("person_delete", kwargs={'id': person.id}))
+
+        # You have to be logged in to access the delete view
+        # We should be redirected to the admin login page
+        self.assertEqual(response.status_code, 302)
+
+        # Login the test user
+        login = self.client.login(username='testuser', password='12345')
+
+        response = self.client.get(reverse("person_delete", kwargs={'id': person.id}))
+        # print(response['Location'])
+        target_url = f"/admin/login/?next=/catalog/person/{person.id}/delete"
+        # print(f"target_url={target_url}")
+
+        # You have to be logged in to access the delete view.
+        # However, only an admin can delete and object so we should be redirected to the admin login page
+        # self.assertEqual(response.status_code, 302)
+        # self.assertEqual(response['Location'], target_url)
+
+        self.assertRedirects(response,
+                             expected_url=target_url,
+                             status_code=302,
+                             target_status_code=200,)
+
+        self.client.logout()
