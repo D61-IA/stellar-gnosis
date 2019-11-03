b0cfd2bdf43c740d6f95ac6a39af9c6b29b22d3d
PantelisElinas <pantelis.elinas@data61.csiro.au>
2019-11-04 09:16:54 +1100

    Updated models.

--- gnosis/notes/models.py
+++ gnosis/notes/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from catalog.models import Paper
 
 
 class Note(models.Model):
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who create the note
-    paper_id = models.IntegerField(null=False, blank=False)  # A paper in the Neo4j DB
+
     note_content = models.TextField()  # The note content
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)  # The user who create the note
+    paper = models.ForeignKey(Paper,
+                              on_delete=models.CASCADE,  # deleting a paper deletes all associated comments
+                              )
+
+    # paper_id = models.IntegerField(null=False, blank=False)  # A paper in the Neo4j DB
     created_at = models.DateTimeField()  # The first time the note was created
     updated_at = models.DateTimeField(auto_now=True)  # The last time the note was modified
 
