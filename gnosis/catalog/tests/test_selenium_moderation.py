4d1e3e9576d456da4ada9710f73995001579153f
Zhenghao-Zhao <grekinaus@gmail.com>
2019-12-05 00:03:01 +1100

    temp save

--- gnosis/catalog/tests/test_selenium_moderation.py
+++ gnosis/catalog/tests/test_selenium_moderation.py
@@ -10,54 +10,62 @@ from django.contrib.auth.models import User
 
 class ChromeTestCase(unittest.TestCase):
 
-
     def setupBrowser(self):
         """set up testing browser"""
 
         self.browser = webdriver.Chrome()
 
-    def createUsers(self):
-        """create two users, user2 as admin whose info will be used for logging in"""
-
-        self.username = 'user1'
-        self.userpassword = '12345'
-        self.useremail = 'somthing@gnosis.stellargraph.io'
-
-        self.user2name = 'user2'
-        self.user2password = 'abcde'
-        self.user2email = 'somethingelse@gnosis.stellargraph.io'
-
-        self.user = User.objects.create_user(username=self.username, password=self.userpassword,
-                                             email=self.useremail)
-
-        # create user 2 who is an admin
-        self.user2 = User.objects.create_superuser(username=self.user2name,
-                                                   password=self.user2password,
-                                                   email=self.user2email)
+    # def createUsers(self):
+    #     """create two users, user2 as admin whose info will be used for logging in"""
+    #
+    #     self.username = 'user1'
+    #     self.userpassword = '12345'
+    #     self.useremail = 'user1@gnosis.stellargraph.io'
+    #
+    #     self.user2name = 'user2'
+    #     self.user2password = 'abcde'
+    #     self.user2email = 'user2@gnosis.stellargraph.io'
+    #
+    #     self.user = User.objects.create_user(username=self.username, password=self.userpassword,
+    #                                          email=self.useremail)
+    #
+    #     # create user 2 who is an admin
+    #     self.user2 = User.objects.create_superuser(username=self.user2name,
+    #                                                password=self.user2password,
+    #                                                email=self.user2email)
 
     @classmethod
     def setUpClass(cls):
-        cls.usrname = 'testuser130'
-        cls.adminname = 'admin30'
 
-        cls.user = User.objects.create_user(username=cls.usrname, password="12345")
+        # create a regular user and an admin user
+        cls.user1name = 'user1'
+        cls.user1password = '12345'
+        cls.user1email = 'user1@gnosis.stellargraph.io'
 
-        cls.admin = User.objects.create_superuser(username=cls.adminname,
-                                                  password='abcdefg',
-                                                  email="admin@gnosis.stellargraph.io")
-        # Create a paper
+        cls.user2name = 'user2'
+        cls.user2password = 'abcde'
+        cls.user2email = 'user2@gnosis.stellargraph.io'
+
+        cls.user1 = User.objects.create_user(username=cls.user1name, password=cls.user1password,
+                                             email=cls.user1email)
+
+        cls.user2 = User.objects.create_superuser(username=cls.user2name,
+                                                  password=cls.user2password,
+                                                  email=cls.user2email)
+        # create a paper
         cls.paper = Paper.objects.create(
             title="Best paper in the world",
             abstract="The nature of gravity.",
             download_link="https://google.com",
-            created_by=cls.user,
+            created_by=cls.user1,
         )
 
     def setUp(self):
 
+        # in setup because comment will be deleted in a test
         self.comment = Comment.objects.create(
             text="testing comment",
-            created_by=self.user,
+            created_by=self.user1,
             is_flagged=False,
             is_hidden=False,
             paper=self.paper
@@ -68,11 +76,11 @@ class ChromeTestCase(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000/accounts/login/?next=/catalog/paper/' + str(self.paper.id) + '/')
         username = self.browser.find_element_by_id('id_login')
         username.clear()
-        username.send_keys(self.adminname)
+        username.send_keys(self.user2name)
 
         pwd = self.browser.find_element_by_id('id_password')
         pwd.clear()
-        pwd.send_keys('abcdefg')
+        pwd.send_keys(self.user2password)
 
         self.browser.find_element_by_tag_name('form').submit()
 
@@ -85,7 +93,7 @@ class ChromeTestCase(unittest.TestCase):
         a = self.first_comment.find_element_by_tag_name('a')
         a.click()
 
-        # completing and submitting a flagging form
+        # completing and submitting a flag form
         flag_form = self.browser.find_element_by_tag_name('form')
 
         violations = flag_form.find_element_by_id('id_violation')
@@ -340,6 +348,7 @@ class FirefoxTestCase(unittest.TestCase):
 
         self.assertEqual(len(self.comments), 0)
         self.browser.quit()
+
 
 #
 # class EdgeTestCase(unittest.TestCase):
