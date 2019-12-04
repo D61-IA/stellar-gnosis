1c5ca2693b1c0d0a3aa3d25e2e1e896215b757fc
Zhenghao-Zhao <grekinaus@gmail.com>
2019-12-04 13:39:13 +1100

    reformat flaggingcomment selenium tests

--- gnosis/catalog/tests/test_selenium_flaggingcomments.py
+++ gnosis/catalog/tests/test_selenium_flaggingcomments.py
@@ -8,90 +8,117 @@ from selenium.webdriver.support import expected_conditions as EC
 from django.contrib.auth.models import User
 
 
-class GoogleTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+class ChromeTestCase(unittest.TestCase):
 
-        # create temporary assets for testing
-        cls.usrname = 'testuser118'
-        cls.adminname = 'admin18'
+    # create two users
+    def createUsers(self):
+        self.username = 'testusr'
+        self.userpassword = '12345'
+        self.useremail = 'testusr@gnosis.stellargraph.io'
 
-        cls.user = User.objects.create_user(username=cls.usrname, password="12345")
+        self.user2name = 'testusr2'
+        self.user2password = 'abcde'
+        self.user2email = 'testusr2@gnosis.stellargraph.io'
 
-        cls.admin = User.objects.create_superuser(username=cls.adminname,
-                                                  password='abcdefg',
-                                                  email="admin@gnosis.stellargraph.io")
+        self.user = User.objects.create_user(username=self.username, password=self.userpassword,
+                                             email=self.useremail)
 
-        cls.paper = Paper.objects.create(
+        self.user2 = User.objects.create_superuser(username=self.user2name,
+                                                   password=self.user2password,
+                                                   email=self.user2email)
+
+    # create a paper
+    def createPaper(self):
+        self.paper = Paper.objects.create(
             title="Best paper in the world",
             abstract="The nature of gravity.",
             download_link="https://google.com",
-            created_by=cls.user,
+            created_by=self.user,
         )
 
-        cls.comment = Comment.objects.create(
+    # create a comment, with is_flagged as False
+    def createComment(self):
+        self.comment = Comment.objects.create(
             text="testing comment",
-            created_by=cls.user,
+            created_by=self.user,
             is_flagged=False,
             is_hidden=False,
-            paper=cls.paper
+            paper=self.paper
         )
 
-        # set testing browser to Chrome
-        cls.browser = webdriver.Chrome()
-        # login as admin
-        cls.browser.get('http://127.0.0.1:8000/accounts/login/?next=/catalog/paper/' + str(cls.paper.id) + '/')
-        username = cls.browser.find_element_by_id('id_login')
+    # create all necessary assets
+    def createAssets(self):
+        self.createUsers()
+        self.createPaper()
+        self.createComment()
+
+    # fill in the flag form
+    def fillFlagForm(self, flag_form):
+        # select the first violation
+        input_0 = flag_form.find_element_by_id('id_violation_0')
+        input_0.click()
+        # find description text area and insert text
+        description = flag_form.find_element_by_id('id_description')
+        description.clear()
+        description.send_keys('violation description')
+
+    # setup testing browser
+    def setupBrowser(self):
+        self.browser = webdriver.Chrome()
+
+    def setUp(self):
+        # create temporary assets for testing
+        self.createAssets()
+        self.setupBrowser()
+
+        # login as user
+        self.browser.get('http://127.0.0.1:8000/accounts/login/?next=/catalog/paper/' + str(self.paper.id) + '/')
+        username = self.browser.find_element_by_id('id_login')
         username.clear()
-        username.send_keys(cls.adminname)
+        username.send_keys(self.user2name)
 
-        pwd = cls.browser.find_element_by_id('id_password')
+        pwd = self.browser.find_element_by_id('id_password')
         pwd.clear()
-        pwd.send_keys('abcdefg')
+        pwd.send_keys(self.user2password)
 
-        cls.browser.find_element_by_tag_name('form').submit()
-
-        cls.comment_container = cls.browser.find_element_by_css_selector('ul.list-group')
+        self.browser.find_element_by_tag_name('form').submit()
+        # wait for Ajax response
+        wait = WebDriverWait(self.browser, 10)
+        element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'ul.list-group')))
+        self.comment_container = self.browser.find_element_by_css_selector('ul.list-group')
         # there should be only one comment in this fictional paper
-        cls.first_comment = cls.comment_container.find_element_by_css_selector('li.list-group-item')
+        self.first_comment = self.comment_container.find_element_by_css_selector('li.list-group-item')
 
     # remove temporary assets from DB
-    @classmethod
-    def tearDownClass(cls):
-        cls.user.delete()
+    def tearDown(self):
+        self.user.delete()
+        self.user2.delete()
+        self.paper.delete()
+        self.comment.delete()
+        self.browser.quit()
 
-        cls.admin.delete()
+    # test all actions for flagging a comment
+    def test_flaggingcomments(self):
+        first_comment = self.first_comment
+        browser = self.browser
+        flag_form_container = browser.find_element_by_id('flag_form_container')
 
-        cls.paper.delete()
-
-        cls.comment.delete()
-
-    # the following tests are designed to test on comments that are yet flagged, make sure the page has such comments.
-    # assert each comment has the correct flag status shown on the flag UI
-    def testFlagUIs(self):
-        text = self.first_comment.find_element_by_class_name('material-icons').text
+        # test the flag is an outlined flag for unflagged comment
+        text = first_comment.find_element_by_class_name('material-icons').text
         self.assertEqual(text, 'outlined_flag')
 
-    # assert pop up flag form is shown when flag icon is clicked
-    def testFlagClick(self):
-        first_comment = self.first_comment
+        # assert pop up flag form is shown when flag icon is clicked
         flag_clickable = first_comment.find_element_by_class_name('open_flag_dialog')
         flag_clickable.click()
-
-        a1 = self.browser.find_element_by_id('flag_form_container').get_attribute('hidden')
+        a1 = flag_form_container.get_attribute('hidden')
         self.assertEqual(a1, None)
 
-    def testFlagForm(self):
-        flag_form = self.browser.find_element_by_id('flag_form')
+        # assert flag form has the right fields
+        flag_form = browser.find_element_by_id('flag_form')
         violations = flag_form.find_element_by_id('id_violation')
         labels = violations.find_elements_by_tag_name('label')
-        form = FlaggedCommentForm()
-
-        true_violations = form.fields['violation'].choices
-        true_labels = []
-
-        for v in true_violations:
-            true_labels.append(v[0])
+        true_violations = FlaggedCommentForm().fields['violation'].choices
+        true_labels = [v[0] for v in true_violations]
 
         # assert radio buttons have the right violations.
         for i in range(len(labels)):
@@ -103,182 +130,44 @@ class GoogleTestCase(unittest.TestCase):
         desc_id = description[0].get_attribute('id')
         self.assertEqual(desc_id, 'id_description')
 
-    def testFlagValidFormSubmit(self):
-
-        flag_form = self.browser.find_element_by_id('flag_form')
-
         # find first select option and select
-        input_0 = flag_form.find_element_by_id('id_violation_0')
-        input_0.click()
+        self.fillFlagForm(flag_form)
 
-        # find description text area and insert text
-        description = flag_form.find_element_by_id('id_description')
-        description.clear()
-        description.send_keys('violation description')
+        # assert the cancel button works
+        flag_form.find_element_by_tag_name('button').click()
+        self.assertEqual(flag_form_container.get_attribute('hidden'), 'true')
 
-        flag_form.submit()
+        # assert the choice and description are cleared
+        choice = flag_form.find_element_by_id('id_violation_0')
+        self.assertFalse(choice.is_selected())
+        text = flag_form.find_element_by_id('id_description')
+        self.assertFalse(text.get_attribute('value'))
 
-        # wait for Ajax response
-        wait = WebDriverWait(self.browser, 10)
-        element = wait.until(EC.visibility_of_element_located((By.ID, 'flag_response')))
-
-        # after submit, assert flag form is hidden
-        a1 = self.browser.find_element_by_id('flag_form_container').get_attribute('hidden')
-        self.assertEqual(a1, 'true')
-
-        # assert flag_response is unhidden after successful submit
-        flag_response = self.browser.find_element_by_id('flag_response')
-        self.assertEqual(flag_response.get_attribute('hidden'), None)
-
-    def testFlaggedComment(self):
-        first_comment = self.first_comment
-        arr = first_comment.find_elements_by_class_name("flagged")
-        self.assertEqual(len(arr), 1)
-
-        arr = first_comment.find_elements_by_class_name("material-icons")
-        self.assertEqual(arr[0].text, "flag")
-        self.browser.quit()
-
-
-
-class FirefoxTestCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-
-        # create temporary assets for testing
-        cls.usrname = 'testuser119'
-        cls.adminname = 'admin19'
-
-        cls.user = User.objects.create_user(username=cls.usrname, password="12345")
-
-        cls.admin = User.objects.create_superuser(username=cls.adminname,
-                                                  password='abcdefg',
-                                                  email="admin@gnosis.stellargraph.io")
-
-        cls.paper = Paper.objects.create(
-            title="Best paper in the world",
-            abstract="The nature of gravity.",
-            download_link="https://google.com",
-            created_by=cls.user,
-        )
-
-        cls.comment = Comment.objects.create(
-            text="testing comment",
-            created_by=cls.user,
-            is_flagged=False,
-            is_hidden=False,
-            paper=cls.paper
-        )
-
-        # set testing browser to Chrome
-        cls.browser = webdriver.Firefox()
-        # login as admin
-        cls.browser.get('http://127.0.0.1:8000/accounts/login/?next=/catalog/paper/' + str(cls.paper.id) + '/')
-        username = cls.browser.find_element_by_id('id_login')
-        username.clear()
-        username.send_keys(cls.adminname)
-
-        pwd = cls.browser.find_element_by_id('id_password')
-        pwd.clear()
-        pwd.send_keys('abcdefg')
-
-        cls.browser.find_element_by_tag_name('form').submit()
-
-        # wait for Ajax response
-        wait = WebDriverWait(cls.browser, 10)
-        element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'ul.list-group')))
-
-        cls.comment_container = cls.browser.find_element_by_css_selector('ul.list-group')
-        # there should be only one comment in this fictional paper
-        cls.first_comment = cls.comment_container.find_element_by_css_selector('li.list-group-item')
-
-    # remove temporary assets from DB
-    @classmethod
-    def tearDownClass(cls):
-        cls.user.delete()
-
-        cls.admin.delete()
-
-        cls.paper.delete()
-
-        cls.comment.delete()
-
-    # the following tests are designed to test on comments that are yet flagged, make sure the page has such comments.
-    # assert each comment has the correct flag status shown on the flag UI
-    def testFlagUIs(self):
-        text = self.first_comment.find_element_by_class_name('material-icons').text
-        self.assertEqual(text, 'outlined_flag')
-
-    # assert pop up flag form is shown when flag icon is clicked
-    def testFlagClick(self):
-        first_comment = self.first_comment
-        flag_clickable = first_comment.find_element_by_class_name('open_flag_dialog')
+        # reopen the form
         flag_clickable.click()
-
-        a1 = self.browser.find_element_by_id('flag_form_container').get_attribute('hidden')
-        self.assertEqual(a1, None)
-
-    def testFlagForm(self):
-        flag_form = self.browser.find_element_by_id('flag_form')
-        violations = flag_form.find_element_by_id('id_violation')
-        labels = violations.find_elements_by_tag_name('label')
-        form = FlaggedCommentForm()
-
-        true_violations = form.fields['violation'].choices
-        true_labels = []
-
-        for v in true_violations:
-            true_labels.append(v[0])
-
-        # assert radio buttons have the right violations.
-        if labels is not None:
-            for i in range(len(labels)):
-                self.assertEqual(true_labels[i], labels[i].text)
-        else:
-            print("Form radio buttons not found.")
-
-        # assert the form has the right description field
-        description = flag_form.find_elements_by_tag_name('textarea')
-        self.assertEqual(len(description), 1)
-        desc_id = description[0].get_attribute('id')
-        self.assertEqual(desc_id, 'id_description')
-
-    def testFlagValidFormSubmit(self):
-
-        flag_form = self.browser.find_element_by_id('flag_form')
-
-        # find first select option and select
-        input_0 = flag_form.find_element_by_id('id_violation_0')
-        input_0.click()
-
-        # find description text area and insert text
-        description = flag_form.find_element_by_id('id_description')
-        description.clear()
-        description.send_keys('violation description')
+        self.fillFlagForm(flag_form)
 
         flag_form.submit()
-
         # wait for Ajax response
-        wait = WebDriverWait(self.browser, 10)
+        wait = WebDriverWait(browser, 10)
         element = wait.until(EC.visibility_of_element_located((By.ID, 'flag_response')))
-
         # after submit, assert flag form is hidden
-        a1 = self.browser.find_element_by_id('flag_form_container').get_attribute('hidden')
+        a1 = browser.find_element_by_id('flag_form_container').get_attribute('hidden')
         self.assertEqual(a1, 'true')
-
         # assert flag_response is unhidden after successful submit
-        flag_response = self.browser.find_element_by_id('flag_response')
+        flag_response = browser.find_element_by_id('flag_response')
         self.assertEqual(flag_response.get_attribute('hidden'), None)
 
-    def testFlaggedComment(self):
-        first_comment = self.first_comment
+        # assert the flagged comment has the right flag icon
         arr = first_comment.find_elements_by_class_name("flagged")
         self.assertEqual(len(arr), 1)
-
         arr = first_comment.find_elements_by_class_name("material-icons")
         self.assertEqual(arr[0].text, "flag")
-        self.browser.quit()
 
+
+class FirfoxTestCase(ChromeTestCase):
+    def setupBrowser(self):
+        self.browser = webdriver.Firefox()
 
 
 # class EdgeTestCase(unittest.TestCase):
