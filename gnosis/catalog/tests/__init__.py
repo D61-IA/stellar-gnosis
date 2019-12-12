0486ebf5f38cf725d2ab8012bedaadb128bfb6a9
Zhenghao-Zhao <grekinaus@gmail.com>
2019-12-13 10:28:00 +1100

    add import statements to init files of subfolders, remove import statements from init under tests

--- gnosis/catalog/tests/__init__.py
+++ gnosis/catalog/tests/__init__.py
@@ -1,16 +1,2 @@
-from catalog.tests.django_tests.test_author_model import *
-from catalog.tests.django_tests.test_author_views import *
-from catalog.tests.django_tests.test_venue_model import *
-from catalog.tests.django_tests.test_venue_views import *
-from catalog.tests.django_tests.test_dataset_model import *
-from catalog.tests.django_tests.test_dataset_views import *
-from catalog.tests.django_tests.test_code_model import *
-from catalog.tests.django_tests.test_code_views import *
-from catalog.tests.django_tests.test_group_views import *
-from catalog.tests.django_tests.test_bookmark_view import *
-from catalog.tests.django_tests.test_endorsement_view import *
-from catalog.tests.selenium_tests.test_flaggingcomments import *
-from catalog.tests.selenium_tests.test_moderation import *
-from catalog.tests.selenium_tests.test_bookmark_endorsement import *
 
 
