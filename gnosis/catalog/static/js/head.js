86b3d26f3571ed6ca9fd682715e80a219a3ef615
Zhenghao-Zhao <grekinaus@gmail.com>
2019-12-29 23:49:28 +1100

    add more indexes to pagination when current index is within 2 to either first or last page

--- gnosis/catalog/static/js/head.js
+++ gnosis/catalog/static/js/head.js
@@ -1,5 +1,54 @@
 function pagination(current, first, last) {
 
+    var $page_items = $('.num_item');
+
+    var path = '?page=';
+
+    if (last > 5) {
+        if (current <= first + 2) {
+            $($page_items).each(function (index) {
+                if (index === 4) {
+                    $(this).children('a').text(last).attr('href', path + last);
+                } else {
+                    $(this).children('a').text(index + 1).attr('href', path + (index + 1));
+                    if (current === index + 1) {
+                        $(this).addClass('active');
+                    }
+                }
+            })
+        } else if (current >= last - 2) {
+            $($page_items).each(function (index) {
+                if (index === 0) {
+                    $(this).children('a').text(first).attr('href', path + first);
+                } else {
+                    $(this).children('a').text(last - 4 + index).attr('href', path + (last - 4 + index));
+                    if (current === last - 4 + index) {
+                        $(this).addClass('active');
+                    }
+                }
+            })
+        } else {
+            $page_items.eq(0).children('a').text(first).attr('href', path + first);
+            $page_items.eq(1).children('a').text((current - 1)).attr('href', path + (current - 1));
+            $page_items.eq(2).children('a').text((current)).attr('href', path + (current));
+            $page_items.eq(2).addClass('active');
+            $page_items.eq(3).children('a').text((current + 1)).attr('href', path + (current + 1));
+            $page_items.eq(4).children('a').text(last).attr('href', path + last);
+        }
+
+    } else {
+        $($page_items).each(function (index) {
+            if (index + 1 <= last) {
+                $(this).children('a').text(index + 1).attr('href', path + (index+1));
+                if (current === index + 1) {
+                    $(this).addClass('active');
+                }
+            } else {
+                $(this).css('display', 'none');
+            }
+        });
+    }
+
     if (current <= first + 2) {
         $('.first_ellipsis').css('display', 'none');
     }
@@ -7,4 +56,5 @@ function pagination(current, first, last) {
     if (current >= last - 2) {
         $('.last_ellipsis').css('display', 'none');
     }
+
 }
